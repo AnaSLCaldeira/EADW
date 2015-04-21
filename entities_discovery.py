4df16b648de52c13f82da57609181a53a22f4475
AnaSLCaldeira <anaslcaldeira@gmail.com>
2015-04-21 19:03:36 +0100

    Melhoria da extracção de entidades com base nos conselhos do professor.

--- entities_discovery.py
+++ entities_discovery.py
@@ -1,38 +1,52 @@
 from news_searcher import *
 import nltk
 from nltk.tree import *
+from nltk.corpus import floresta
+from nltk.corpus import mac_morpho
 
-
-def extract_entities(myTree):
-    myPhrases = []
-    if (myTree.label() == "NE"):
-        myPhrases.append( myTree.copy(True) )
-    for child in myTree:
-        if (type(child) is Tree):
-            list_of_phrases = extract_entities(child)
-            if (len(list_of_phrases) > 0):
-                myPhrases.extend(list_of_phrases)
-    return myPhrases
+def parse_dbpedia():
+	entities = []
+	with open ("entities/DBpediaEntities-PT-0.1/locations.txt", 'r') as f:
+		for line in f:
+			entities.append(line.split("/")[4].replace("_", " ").replace("\n", ""))
+	with open ("entities/DBpediaEntities-PT-0.1/organizations.txt", 'r') as f:
+		for line in f:
+			entities.append(line.split("/")[4].replace("_", " ").replace("\n", ""))
+	with open ("entities/DBpediaEntities-PT-0.1/persons.txt", 'r') as f:
+		for line in f:
+			entities.append(line.split("/")[4].replace("_", " ").replace("\n", ""))
+	with open ("entities/siglas.txt", 'r') as f:
+		for line in f:
+			entity = line.split(" - ")
+			entities.append(entity[0])
+			entities.append(entity[1])
+	return entities
 
 
 def get_entities(news):
-    tags = []
-    tagged_tokens = nltk.pos_tag(nltk.word_tokenize(news, language="portuguese"))
-    named_entities = extract_entities(nltk.chunk.ne_chunk(tagged_tokens, binary=True))
-    for entity in named_entities:
-        phrase = []
-        for i in range(len(entity)):    #ha entidades com mais do que uma palavra
-            phrase.append(entity[i][0])
-        tags.append(" ".join(phrase))
-    return sorted(set(tags))
-
+	news_entities = []
+	words = nltk.word_tokenize(news, language="portuguese")
+	for index, word in enumerate(words):
+		word = word.encode("UTF-8")
+		if not word[0].isupper():
+			continue
+		else:
+			if index < (len(words)-1) and words[index+1][0].isupper(): # verifica se existe outra palavra com letra maiuscula (muito provavelmente estao relacionadas) - estender para ver mais que 2?
+				if word + " " + words[index+1].encode("UTF-8") in dbpedia_entities:
+					news_entities.append(word + " " + words[index+1].encode("UTF-8"))
+			else:
+				if word in dbpedia_entities: #verifica se a palavra e uma entidade
+					news_entities.append(word)
+	return sorted(set(news_entities))
 
 # CODIGO DE TESTE
-#news = news_searcher("portugal")
-#for entry in news:
-#    entities = " ; ".join(get_entities(entry[0][0] + " " + entry[0][1])) + "\n"
-#    print "Score: " + str(entry[1])
-#    print "Titulo: " + entry[0][0]
-#    print "Descricao: " + entry[0][1]
-#    print "Link: " + entry[0][2]
-#    print "Entidades: " + entities
+dbpedia_entities = parse_dbpedia()
+
+news = news_searcher("portugal")
+for entry in news:
+    entities = " ; ".join(get_entities(entry[0][0] + " " + entry[0][1]))
+    print "Score: " + str(entry[1])
+    print "Titulo: " + entry[0][0]
+    print "Descricao: " + entry[0][1]
+    print "Link: " + entry[0][2]
+    print "Entidades: " + entities + "\n"
--- index_feed.py
+++ index_feed.py
@@ -1,10 +1,13 @@
-import feedparser
 import os
+
+import feedparser
 from whoosh import index
+from whoosh.fields import *
+from whoosh.index import create_in
 
 
-source_dn = feedparser.parse("http://feeds.dn.pt/DN-Portugal")
-source_jn = feedparser.parse("http://feeds.jn.pt/JN-Pais")
+source_dn_all = feedparser.parse("http://feeds.dn.pt/DN-Ultimas") # all news
+source_jn_all = feedparser.parse("http://feeds.jn.pt/JN-ULTIMAS") # all news
 index_dir = "indexdir"
 
 def createIndex():
@@ -18,12 +21,33 @@ def createIndex():
         ix = index.open_dir(index_dir)
 
     writer = ix.writer()
-    for feed in source_dn.entries:
+    for feed in source_dn_all.entries:
         description = feed.summary.split("<img")
-        writer.add_document(title=feed.title, body=description[0], link=feed['feedburner_origlink'])
-    for feed in source_jn.entries:
+        title = feed.title.encode('utf-8')
+        if checkIfDocExists(title, 'DN') is False:
+            with open('dn_news.txt', 'a') as news_file:
+                news_file.write(feed.title.encode('utf-8')+' \n')
+            writer.add_document(title=feed.title, body=description[0], link=feed['feedburner_origlink'])
+
+    for feed in source_jn_all.entries:
         description = feed.summary.split("<img")
-        writer.add_document(title=feed.title, body=description[0], link=feed['feedburner_origlink'])
+        title = feed.title.encode('utf-8')
+        if checkIfDocExists(title, 'JN') is False:
+            with open('jn_news.txt', 'a') as news_file:
+                news_file.write(feed.title.encode('utf-8')+' \n')
+            writer.add_document(title=feed.title, body=description[0], link=feed['feedburner_origlink'])
     writer.commit()
 
-#createIndex()
+
+def checkIfDocExists(title, feed):
+    if feed == "DN":
+        f = open('dn_news.txt', 'r')
+    else:
+        f = open('jn_news.txt', 'r')
+    for line in f:
+        if line.strip() == title:
+            return True
+    return False
+
+
+createIndex()
